4a8f4e2c97701666bb53d9ec271b0226e5ffcee8
laijasmine <jasmine.lai@alumni.ubc.ca>
2019-09-16 16:54:25 -0700

    streamlining process - making parsing individual student files a function - checking for data missing using join

--- consolidating_data.R
+++ consolidating_data.R
@@ -2,8 +2,9 @@ library(here)
 library(tidyverse)
 library(readxl)
 
+inventory <- function(folder){
 # get all the excel files
-files <- list.files(here("data"),full.names = T)
+files <- list.files(here(folder), pattern = "*.xlsx",full.names = T)
 
 # reading a csv file
 # df_csv <- read_csv("2019Sep_Algae SCIE 001.csv", skip = 11)
@@ -24,3 +25,20 @@ df_inventory %>%
 
 # removing duplicates
 df_dup <- distinct(df_inventory)
+
+return(df_dup)
+}
+
+data <- list.files("./data")
+
+result <- data %>% 
+  map(~paste0("./data/",.)) %>% 
+  map_dfr(~inventory(.))
+
+df_result <- result[1:8]
+
+temp <- read_xlsx("2019Sep_Algae SCIE 001_template.xlsx", skip = 10, col_types = c("text","text","text","text","text","text","text","text"))
+
+missing <- anti_join(temp, result, by = "UUID")
+
+test <- full_join(temp, df_result, by = "UUID")
