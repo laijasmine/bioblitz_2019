22df86b67e332ad5b49068c8b13af0ec351dd1a6
laijasmine <jasmine.lai@alumni.ubc.ca>
2020-03-09 16:42:14 -0700

    updated summary statistics to work and template

--- 03_code/consolidating_data_1folder.R
+++ 03_code/consolidating_data_1folder.R
@@ -6,7 +6,8 @@ library(readxl)
 #change some variables
 verified_date <- "03/09/2020"
 xlsx_files_folder <- "./02_data/20200309_data/"
-blank_template <- "2019Sep_Algae SCIE 001_template.xlsx"
+blank_template <- "./00_templates/2020Mar01_UBCAlgae ENVR200 BioBlitz_template.xlsx"
+name_of_results_file <- "04_results/2019_bioblitz_verified_envr200.csv"
   
 #function to take the data and give what was inventoried
 inventory_files <- function(files){
@@ -44,29 +45,31 @@ df_result <- result %>%
   mutate(Verified = verified_date)
 
 # get a blank spreadsheet to compare with
-temp <- read_xlsx(blank_template, skip = 3, col_types = c("text","text","text","text","text","text","text","text","text","text"))
+temp <- read_xlsx(blank_template, skip = 3, 
+                  col_types = c("text","text","text","text","text","text","text","text","text","text"))
 
 #checking the data
 # get all the missing records
 missing <- anti_join(temp, result, by = "UUID")
 
-mt <- missing %>% 
+#summarized results
+summary_of_missing <- missing %>% 
   group_by(Genus,Species) %>% 
   summarise(n())
 
-dr <- df_result %>% 
+summary_of_results <- df_result %>% 
   group_by(Genus,Species) %>% 
   summarise(n())
 
-write_csv(df_result, "04_results/2019_bioblitz_verified_envr200.csv")
+write_csv(df_result, name_of_results_file)
 
 # Import data
 # matching by UUID
 # import only Verified Column
 
 # Checking your work if everything is imported properly
-import <- read_xlsx("data/imported_20191015.xlsx")
-
-filter(import,!(import$`Accession Number` %in% df_result$`Accession Number`))
-
-filter(df_result,!(df_result$`Accession Number` %in% import$`Accession Number`))
+# import <- read_xlsx("data/imported_20191015.xlsx")
+# 
+# filter(import,!(import$`Accession Number` %in% df_result$`Accession Number`))
+# 
+# filter(df_result,!(df_result$`Accession Number` %in% import$`Accession Number`))
