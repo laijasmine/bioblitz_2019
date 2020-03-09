f8821982dda5152a72deb30d3470b88489c6f4c5
laijasmine <jasmine.lai@alumni.ubc.ca>
2020-03-09 15:49:43 -0700

    adding Bioblitz data from ENVR 200

--- 03_code/consolidating_data_1folder.R
+++ 03_code/consolidating_data_1folder.R
@@ -3,16 +3,18 @@ library(tidyverse)
 library(readxl)
 
 # if you need to extract data from emails see save_eml_attachments.py
-
+#change some variables
+verified_date <- "03/09/2020"
+xlsx_files_folder <- "./02_data/20200309_data/"
+blank_template <- "2019Sep_Algae SCIE 001_template.xlsx"
+  
 #function to take the data and give what was inventoried
-inventory <- function(folder){
-  # get all the excel files
-  files <- list.files(here(folder), pattern = "*.xlsx",full.names = T)
+inventory_files <- function(files){
   
   # read in all the files and 
   # skip the first 10 rows (instructions and student names)
   df_all <- files %>% 
-    map_dfr(~read_xlsx(.,skip = 10))
+    map_dfr(~read_xlsx(.,skip = 3))
   
   # take only the rows where it was inventoried (x)
   df_inventory <- df_all %>% 
@@ -30,21 +32,19 @@ inventory <- function(folder){
 }
 
 #how to do this for multiple sets of data (in seperate folders - one folder for each "lab group")
-data <- list.files("./02_data/20200309_data/")
+data <- list.files(xlsx_files_folder, full.names = T)
 
 # reads in all the data, gets the unique number of rows, standardizes the x
 result <- data %>% 
-  map(~paste0("./data/",.)) %>% 
-  map_dfr(~inventory(.)) %>% 
+  map_dfr(~inventory_files(.)) %>% 
   unique()
 
-#fixing the columns so it will rbind
 # and making new column of the date verified
-df_result <- result[1:8] %>% 
-  mutate(Verified = "11/10/2019")
+df_result <- result %>% 
+  mutate(Verified = verified_date)
 
 # get a blank spreadsheet to compare with
-temp <- read_xlsx("2019Sep_Algae SCIE 001_template.xlsx", skip = 10, col_types = c("text","text","text","text","text","text","text","text"))
+temp <- read_xlsx(blank_template, skip = 3, col_types = c("text","text","text","text","text","text","text","text","text","text"))
 
 #checking the data
 # get all the missing records
@@ -58,7 +58,7 @@ dr <- df_result %>%
   group_by(Genus,Species) %>% 
   summarise(n())
 
-write_csv(df_result, "results/2019_bioblitz_verified.csv")
+write_csv(df_result, "04_results/2019_bioblitz_verified_envr200.csv")
 
 # Import data
 # matching by UUID
